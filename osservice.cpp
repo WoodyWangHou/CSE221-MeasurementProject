6841e954caf6a8f3a50e07c55336bf756d4bac91
WoodyWangHou <wanghou92@gmail.com>
2018-04-24 14:26:52 -0700

    writing private helper functions, and testing function for process

--- osservice.cpp
+++ osservice.cpp
@@ -1,10 +1,21 @@
 #include "osservice.h"
-#include <linux/kernel.h>
-#include <pthread.h>
+#include  <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdint>
 
-static void
-uint64_t OsService::getTimeStamp(){
+OsService::OsService(){
+  //constructor
+  OsService::_t_per_cycle = 0;
+  uint64_t start_high;
+  uint64_t start_low;
+
+  uint64_t end_high;
+  uint64_t end_low;
+
+}
+
+static uint64_t OsService::getCPUCycles(){
   uint64_t high;
   uint64_t low;
 
@@ -18,10 +29,50 @@ uint64_t OsService::getTimeStamp(){
   return (((uint64_t)high << 32) | low );
 }
 
-uint64_t OsService::testThreadContextSwitchTime(){
+uint64_t OsService::convertCyclesToTime(uint64_t cycles){
 
 }
 
-uint64_t OsService::testProcContextSwitchTime(){
+uint64_t OsService::testThreadContextSwitchTime(){
+
+
+}
+
+uint64_t OsService::testProcContextSwitchTime(uint64_t iter){
+  int pipes[2];
+  pid_t pid;
+  uint64_t end_buffer;
+  uint64_t start;
+
+  if(pipe(pipes) == -1){
+    cout << "pipes create failed" << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  for(uint64_t i = 0; i < iter; i++){
+    pid = fork();
+    switch(pid){
+      case -1:
+      // fork fail
+      cout << "fork process failed" << "\n";
+      exit(EXIT_FAILURE);
+      break;
+
+      case 0:
+      //child process
+      buffer = OsService::getCPUCycles();
+      write(pipes[1], &end_buffer, sizeof(end_buffer));
+      exit(EXIT_SUCCESS);
+      break;
+
+      default:
+      // parent process
+      start = OsService::getCPUCycles();
+      read(pipes[0], &end_buffer, sizeof(end_buffer));
+    }
+
+
+  }
+
 
 }
--- osservice.hpp
+++ osservice.hpp
@@ -5,7 +5,9 @@
 
 class OsService{
   private:
-    uint64_t getTimeStamp();
+    uint64_t _t_per_cycle;
+    static uint64_t getTimeStamp();
+    uint64_t convertCyclesToTime(uint64_t cycles);
 
   public:
     uint64_t testThreadContextSwitchTime();
