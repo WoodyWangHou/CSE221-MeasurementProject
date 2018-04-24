11b7401d6c3afd048563946923b5cb25d060422c
wanghou92@gmail.com <wanghou92@gmail.com>
2018-04-24 15:18:17 -0700

    in progress

--- osservice.cpp
+++ osservice.cpp
@@ -6,13 +6,33 @@
 
 OsService::OsService(){
   //constructor
-  OsService::_t_per_cycle = 0;
+  OsService::_t_per_cycle_ms = 0;
   uint64_t start_high;
   uint64_t start_low;
 
   uint64_t end_high;
   uint64_t end_low;
 
+// record start time
+  asm   volatile (
+         "CPUID\n\t"/*serialize*/
+         "RDTSC\n\t"/*read the clock*/
+         "mov %%edx, %0\n\t"
+         "mov %%eax, %1\n\t": "=r" (start_high), "=r" (start_low)
+         :: "%rax", "%rbx", "%rcx", "%rdx");
+
+  sleep(SLEEP_TIME_SEC);
+
+  asm volatile ("rdtscp\n\t"
+		  "mov %%edx, %0\n\t"
+		  "mov %%eax, %1\n\t"
+		  "cpuid\n\t"
+		  : "=r" (end_high), "=r" (end_low)
+		  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+  uint64_t start = (((uint64_t)start_high << 32) | start_low );
+  uint64_t end = (((uint64_t)end_high << 32) | end_low );
+  _t_per_cycle_ms = (end - start) / SLEEP_TIME_SEC * SEC_TO_MSEC;
 }
 
 static uint64_t OsService::getCPUCycles(){
@@ -41,38 +61,47 @@ uint64_t OsService::testThreadContextSwitchTime(){
 uint64_t OsService::testProcContextSwitchTime(uint64_t iter){
   int pipes[2];
   pid_t pid;
-  uint64_t end_buffer;
+  uint64_t end; // used as buffer
   uint64_t start;
-
-  if(pipe(pipes) == -1){
-    cout << "pipes create failed" << "\n";
-    exit(EXIT_FAILURE);
-  }
+  uint64_t total_cycles;
 
   for(uint64_t i = 0; i < iter; i++){
+    if(pipe(pipes) == -1){
+      std::cout << "pipes create failed" << "\n";
+      exit(EXIT_FAILURE);
+    }
+
     pid = fork();
     switch(pid){
       case -1:
       // fork fail
-      cout << "fork process failed" << "\n";
+      std::cout << "fork process failed" << "\n";
       exit(EXIT_FAILURE);
       break;
 
       case 0:
       //child process
-      buffer = OsService::getCPUCycles();
-      write(pipes[1], &end_buffer, sizeof(end_buffer));
+      end = OsService::getCPUCycles();
+      write(pipes[1], &end, sizeof(end));
       exit(EXIT_SUCCESS);
       break;
 
       default:
       // parent process
       start = OsService::getCPUCycles();
-      read(pipes[0], &end_buffer, sizeof(end_buffer));
+      read(pipes[0], &end, sizeof(end)); // blocked until child process write its end cycle
+
+      wait(NULL); // wait for child process to finish
+      close(pipes[0]);
+      close(pipes[1]);
     }
 
-
+    total_cycles += end - start; //accumulate cycles
   }
+
+  uint64_t avg_cycles = total_cycles / iter;
+
+
 
 
 }
--- osservice.hpp
+++ osservice.hpp
@@ -2,12 +2,17 @@
 #define OSSERVICE_H // define measurement functions for project 1 cpu parts
 
 #include <stdint>
+#include <vector>
+
+const uint64_t SLEEP_TIME_SEC = 1; // sleep for 1 second
+const uint64_t SEC_TO_MSEC = 1000;
 
 class OsService{
   private:
-    uint64_t _t_per_cycle;
+    uint64_t _t_per_cycle_ms;
     static uint64_t getTimeStamp();
-    uint64_t convertCyclesToTime(uint64_t cycles);
+    static double stddev(std::vector<uint64_t> input);
+    static uint64_t convertCyclesToTime(uint64_t cycles);
 
   public:
     uint64_t testThreadContextSwitchTime();
